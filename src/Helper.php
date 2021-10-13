5434903cb1ab7cadc1c06697bed032f1b795f8de
8ctopus <hello@octopuslabs.io>
2021-10-13 11:32:53 +0500

    added: check if command exists

--- src/Helper.php
+++ src/Helper.php
@@ -83,4 +83,37 @@ class Helper
         else
             return $dir;
     }
+
+    /**
+     * Determine if a command exists on the current environment
+     *
+     * @param string $command The command to check
+     * @return bool True if the command has been found ; otherwise, false.
+     * @author https://stackoverflow.com/a/18540185/10126479
+     */
+    static function commandExists($command)
+    {
+        $whereIsCommand = PHP_OS === 'WINNT' ? 'where' : 'which';
+
+        $process = proc_open("${whereIsCommand} ${command}", [
+                0 => array("pipe", "r"), //STDIN
+                1 => array("pipe", "w"), //STDOUT
+                2 => array("pipe", "w"), //STDERR
+            ],
+            $pipes
+        );
+
+        if ($process === false)
+            return false;
+
+        $stdout = stream_get_contents($pipes[1]);
+        $stderr = stream_get_contents($pipes[2]);
+
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+
+        proc_close($process);
+
+        return $stdout !== '';
+    }
 }
